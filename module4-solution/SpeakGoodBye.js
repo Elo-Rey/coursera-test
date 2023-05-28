1aaac9d004be374b751ae0210cc40870a847ecc5
Elo-Rey <108703646+Elo-Rey@users.noreply.github.com>
2023-05-28 23:05:21 +0100

    Update SpeakGoodBye.js

--- module4-solution/SpeakGoodBye.js
+++ module4-solution/SpeakGoodBye.js
@@ -5,10 +5,10 @@
 // See Lecture 52, part 2
 
 (function (window) {
-   var byeSpeaker = {
+   var byeSpeaker = {};
     byeSpeaker.speak = function(){
    console.log(speakWord + " " + name);
-  }};
+  };
    var speakWord = "Good Bye";
 
   
